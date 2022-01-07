16148e3c0cab52b51b8dc893b4ae9dc499696d63
Ghanshyam Prajapati <89632088+ghanshyam-prajapati-it@users.noreply.github.com>
2022-01-07 09:47:43 +0530

    Add files via upload

---
+++ function2.js
@@ -0,0 +1,10 @@
+// Create function and pass the arguments and get output. 
+
+function xyz(a,b,c)
+{
+    console.log("The sum is = " + (a + b + c));
+    console.log(a - b);
+    console.log(a * b);
+    console.log(a / b);
+}
+xyz(13,2,2);
